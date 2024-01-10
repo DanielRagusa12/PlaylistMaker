960b4fa035be2c150731d418612f5a7545d09b40
Daniel Ragusa <daniel.ragusa129@gmail.com>
2024-01-10 03:30:01 -0500

    Changed directory structure for deployment

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use((req, res, next) => {
 app.use(cors());
 
 
-app.use(express.static(path.join(__dirname, './../frontend/public/')));
-app.set('views', path.join(__dirname, './../frontend/views/'));
+app.use(express.static(path.join(__dirname, './frontend/public/')));
+app.set('views', path.join(__dirname, './frontend/views/'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
