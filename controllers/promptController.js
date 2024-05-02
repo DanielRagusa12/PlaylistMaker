b2c5e91b25e7dde05d9a21b2a4af933356e77762
Daniel Ragusa <daniel.ragusa129@gmail.com>
2024-05-02 14:07:09 -0400

    Temp fix for spotify api response for addSong

--- controllers/promptController.js
+++ controllers/promptController.js
@@ -340,7 +340,7 @@ const addSong = async (req, res) => {
                 position: 0
             }
         });
-        if (response.status === 201) {
+        if (response.status === 200) {
             console.log("song added to playlist");
             res.status(200).render('addSongs');
         }
